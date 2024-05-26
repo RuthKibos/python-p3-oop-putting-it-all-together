b2c535e8bf87109808546300a5b0760ee0bf2fc4
RuthKibos <kibosruth@gmail.com>
2024-05-26 23:22:34 +0300

    Decorators

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    def __init__(self, brand, size):
+       self.brand = brand
+       self.size = size
+       if not isinstance(size, int):
+            raise ValueError("size must be an integer")   
+       self.condition = 'New'
+
+    def cobble(self):
+        self.condition = 'New'
+        print("Your shoe is as good as new!")   
+
+       
     pass
